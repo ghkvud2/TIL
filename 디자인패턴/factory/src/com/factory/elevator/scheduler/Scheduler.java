2c0d0c60c96a1a20651fda5412499a024b3ceff1
ghkvud2 <leeghkvud@gmail.com>
2019-09-01 15:51:33 +0900

    팩토리 메서드 패턴

---
+++ 디자인패턴/factory/src/com/factory/elevator/scheduler/Scheduler.java
@@ -0,0 +1,8 @@
+package com.factory.elevator.scheduler;
+
+import com.factory.elevator.enums.Direction;
+import com.factory.elevator.manager.ElevatorManager;
+
+public interface Scheduler {
+    int selectElevator(ElevatorManager elevatorManager, int destination, Direction direction);
+}
