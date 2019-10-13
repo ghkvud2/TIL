e719b860166828bc111dabdcd32c04ae790423f6
ghkvud2 <leeghkvud@gmail.com>
2019-10-14 00:39:13 +0900

    prime number example

---
+++ 클린코드/CleanCode/src/test/java/com/oop/cleancode/primenumber/PrintPrimesTest.java
@@ -0,0 +1,37 @@
+package com.oop.cleancode.primenumber;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class PrintPrimesTest {
+    private PrintStream out;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        out = System.out;
+        System.setOut(new PrintStream(new FileOutputStream("lead")));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(out);
+        new File("lead").delete();
+    }
+
+    @Test
+    public void
+    makeSureMatchesGold() throws IOException {
+        new PrintPrimes().main(new String[0]);
+        BufferedReader lead = new BufferedReader(new FileReader("lead"));
+        BufferedReader gold = new BufferedReader(new FileReader("src/test/java/com/oop/cleancode/primenumber/gold"));
+        String line;
+        while((line = gold.readLine()) != null)
+            assertEquals(line, lead.readLine());
+        assertEquals(null, lead.readLine());
+    }
+}
